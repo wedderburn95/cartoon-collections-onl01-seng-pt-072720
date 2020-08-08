236f4f0246fbc0e7eb00215a661ee0c2bed4ea80
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-08 06:40:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,11 @@ end
 
 roll_call_dwarves(dwarves)
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
 end
+
+summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls# code an argument here
   # Your code here
