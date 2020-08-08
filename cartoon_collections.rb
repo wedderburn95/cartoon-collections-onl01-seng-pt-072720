1561f0a18f5d7882c11b0da083bb72202c0a70f2
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-08 06:41:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,11 @@ end
 
 summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
+end
+
+long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese# code an argument here
