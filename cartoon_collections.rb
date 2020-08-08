9b5b41aa1e22a872c1b4ecdcc8dae07a68228c31
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-08 06:48:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,6 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 long_planeteer_calls(planeteer_calls)
-end
 
 def find_the_cheese(potentially_cheesy_items)
   cheeses = %w[gouda cheddar camembert]
